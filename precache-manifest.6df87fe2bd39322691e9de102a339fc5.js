be427846184530ccdd3137f3429199d2ceb3517d
louannem <84766775+louannem@users.noreply.github.com>
2022-04-02 21:08:23 +0200

    Updates

---
+++ precache-manifest.6df87fe2bd39322691e9de102a339fc5.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46c3c804cb55c10b77e2e8ecf68ba93c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "0645352bf1f6ddd3f718",
+    "url": "./static/css/main.b75ca4e1.chunk.css"
+  },
+  {
+    "revision": "d55b725a65b99bc24f19",
+    "url": "./static/js/2.c2c3a895.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.c2c3a895.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0645352bf1f6ddd3f718",
+    "url": "./static/js/main.fd139978.chunk.js"
+  },
+  {
+    "revision": "68aff7cb672dffa8e7dd",
+    "url": "./static/js/runtime-main.5b5dfc1b.js"
+  },
+  {
+    "revision": "dcfbb135503f76dd270f9b13e8dc7bd2",
+    "url": "./static/media/calendar-icon~HOgszezb.dcfbb135.svg"
+  },
+  {
+    "revision": "ea8ab7b698d6c82feff90456cf6d0af1",
+    "url": "./static/media/left-chevron~MYFfYiLI.ea8ab7b6.svg"
+  },
+  {
+    "revision": "6eb1650982a658de25170c6eb78ea244",
+    "url": "./static/media/left-double-chevron~MMhjQrUz.6eb16509.svg"
+  },
+  {
+    "revision": "d6fe6a980707149485e99a6e8454feae",
+    "url": "./static/media/right-chevron~FLrhNaXD.d6fe6a98.svg"
+  },
+  {
+    "revision": "0ab7dabcf673391f057e2d598b0c50eb",
+    "url": "./static/media/right-double-chevron~bjGHbmUM.0ab7dabc.svg"
+  }
+]);
