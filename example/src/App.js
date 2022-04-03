7c5f5b9bb6e22f443af45519a0995aa5f6de92db
louannem <84766775+louannem@users.noreply.github.com>
2022-04-03 22:18:22 +0200

    Update App.js

--- example/src/App.js
+++ example/src/App.js
@@ -19,40 +19,42 @@ export default function App() {
       <div className='componentExamplesWrapper'>
           <h1>Datepicker plugin examples</h1>
 
-          <div>
-              <h2>Basic datepicker</h2>
-              <DatePicker onChange={handleChange} />
-          </div>
+            <div className='examples-grid'>
+                <div>
+                    <h2>Basic datepicker</h2>
+                    <DatePicker onChange={handleChange} />
+                </div>
 
-          <div>
-              <h2>Datepicker with icon</h2>
-              <DatePicker inputIcon={true} />
-          </div>
+                <div>
+                    <h2>Datepicker with icon</h2>
+                    <DatePicker inputIcon={true} />
+                </div>
 
-          <div>
-              <h2>Hiding the close button</h2>
-              <DatePicker  closeButton={false} />
-          </div>
+                <div>
+                    <h2>Hiding the close button</h2>
+                    <DatePicker  closeButton={false} />
+                </div>
 
-          <div>
-              <h2>Ignoring today's date (no highlight)</h2>
-              <DatePicker  hightlightToday={false} />
-          </div>
+                <div>
+                    <h2>Ignoring today's date (no highlight)</h2>
+                    <DatePicker  hightlightToday={false} />
+                </div>
 
-          <div>
-              <h2>Personnalized starting month</h2>
-              <DatePicker  startMonth={0} />
-          </div>
+                <div>
+                    <h2>Personnalized starting month</h2>
+                    <DatePicker  startMonth={0} />
+                </div>
 
-          <div>
-              <h2>Personnalized starting year</h2>
-              <DatePicker  startYear={2000} />
-          </div>
+                <div>
+                    <h2>Personnalized starting year</h2>
+                    <DatePicker  startYear={2000} />
+                </div>
 
-          <div>
-              <h2>Switching the language to french</h2>
-              <DatePicker  lang="fr" /> 
-          </div>
+                <div>
+                    <h2>Switching the language to french</h2>
+                    <DatePicker  lang="fr" /> 
+                </div>
+            </div>
       </div>
   )
 }
